860721a32ffbc9d40169ccf1b2e18610dc9b5884
Vicky <saivicky2324@gmail>
2024-02-07 20:32:10 +0530

    m

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import ForgotPassword from "./Pages/Authentication/ForgotPassword"
 import Auth from "./Pages/Authentication/Auth"
+import UniversitiesList from "./Pages/Universities/UniversitiesList"
 
 const Stack = createNativeStackNavigator();
 
@@ -14,17 +15,18 @@ export default function App() {
   return (
 			<NavigationContainer>
 					<Stack.Navigator
-				   	initialRouteName="Login"
+				   	initialRouteName="Universities List"
 								screenOptions = {{
 									headerTintColor: 'white',
 									headerTitleStyle: styles.headerTitleStyle,
 									headerMode: 'float',
-									headerShown: false,
+									headerShown: true,
 									headerStyle: {
 							backgroundColor: '#0384d5',
 					},}}>
 							<Stack.Screen name="Login" component={Auth} />
 							<Stack.Screen name="Forgot Password" component={ForgotPassword} />
+							<Stack.Screen name="Universities List" component={UniversitiesList} />
 					</Stack.Navigator>
 			</NavigationContainer>
   );
--- Pages/Universities/UniversitiesList.js
+++ Pages/Universities/UniversitiesList.js
@@ -1,11 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Dimensions } from 'react-native'
 import React from 'react'
+import Icon from 'react-native-vector-icons/Ionicons';
 
+const {height,width} = Dimensions.get('window')
 const UniversitiesList = () => {
 		return (
-				<View>
-						<Text>UniversitiesList</Text>
+			<View style={renderStyles.renderView}>
+			<View style={renderStyles.txtBox}>
+				<View style={renderStyles.profileCircle}>
+				<Icon name="person" size={18}/>
 				</View>
+				<Text style={renderStyles.txtBody}>Sai krishna </Text>
+			</View>
+			<View style={{alignItems:"flex-end"}}>
+			<Text style={renderStyles.txtBody3}>Reg.Date:  </Text>
+	</View>
+			<View style={renderStyles.txtBox1}>
+			<Text style={renderStyles.txtBody1}> krishna</Text>
+				<Text style={renderStyles.txtBody2}>krishna </Text>
+	</View>
+
+</View>
 		)
 }
 
@@ -13,5 +28,59 @@ export default UniversitiesList
 
 const styles = StyleSheet.create({
 
-	
+
+})
+
+const renderStyles = StyleSheet.create({
+	renderView :{
+	   backgroundColor:"white",
+				alignSelf:'center',
+				width:width*0.9,
+				height:height*0.2,
+				elevation:5,
+				margin:10
+	},
+	txtBox:{
+		marginLeft:20,
+		marginTop:20,
+		flexDirection:"row"
+},
+txtBody:{
+	fontSize:18,
+	color:"black",
+	width:width*0.8,
+	top:5
+	},
+	txtBox1:{
+		width:width*0.9,
+		flexDirection:"row",
+		justifyContent:"space-evenly",
+		alignSelf:'center',
+		marginTop:20
+},
+txtBody1:{
+	fontSize:18,
+	color:"black",
+	width:width*0.4,
+	},
+	txtBody2:{
+		fontSize:18,
+		color:"black",
+		width:width*0.4,
+		},
+		profileCircle:{
+			width:  width*0.08,
+			height: width*0.08,
+			borderRadius: width/10,
+			backgroundColor:"#edeef6",
+			justifyContent:"center",
+			// opacity:0.5,
+			alignItems:"center",
+			marginRight:15
+		},
+		txtBody3:{
+			fontSize:16,
+			color:"#c8c8c8",
+			width:width*0.4,
+			},
 })
